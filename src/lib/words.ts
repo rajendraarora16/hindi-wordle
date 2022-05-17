83d32bb7f2ec0bb8b66facd68a977b59263dfbf3
Rajendra arora <contact.rajendraarora@gmail.com>
2022-05-17 11:53:45 +0530

    add new epoch time

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -13,8 +13,8 @@ export const isWinningWord = (word: string) => {
 }
 
 export const getWordOfDay = () => {
-  // May 3, 2022 Game Epoch
-  const epochMs = 1651547271296
+  // May 17, 2022 Game Epoch
+  const epochMs = 1652768596995
   const now = Date.now()
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
