8c2044299aa4df1f3df0fcc0fdad8f31f585382b
Rajendra arora <contact.rajendraarora@gmail.com>
2022-05-25 07:55:02 +0530

    added recent epoch timestamp

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -13,8 +13,8 @@ export const isWinningWord = (word: string) => {
 }
 
 export const getWordOfDay = () => {
-  // May 17, 2022 Game Epoch
-  const epochMs = 1652768596995
+  // May 25, 2022 Game Epoch
+  const epochMs = 1653445440343
   const now = Date.now()
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
